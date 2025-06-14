51393e11d0224d1dfa8450fe5b8991002e0ddee3
Merdancik94 <97890006+Merdancik94@users.noreply.github.com>
2025-06-15 02:08:01 +0500

    Update 3x.sh

--- 3x.sh
+++ 3x.sh
@@ -11,13 +11,16 @@ echo -e "${green}Модифицирую скрипт для автоматиче
 
 cp install-x-ui-original.sh install-x-ui-auto.sh
 
-# Удаляем весь старый if-блок с выбором порта (от начала read до строки с "/usr/local/x-ui/x-ui setting")
+# Полная замена интерактивного блока на автоматический
 sed -i '/read -rp "Would you like to customize the Panel Port settings?/,/\/usr\/local\/x-ui\/x-ui setting -username/c\
             local config_port=$(shuf -i 1024-62000 -n 1)\n\
             echo -e "${yellow}Automatically selected random port: ${config_port}${plain}"\n\
             /usr/local/x-ui/x-ui setting -username \"${config_username}\" -password \"${config_password}\" -port \"${config_port}\" -webBasePath \"${config_webBasePath}\"' install-x-ui-auto.sh
 
-# Делаем исполняемым и запускаем
+# Удалим возможные символы \r, если они были (на случай повторного запуска из Windows-среды)
+sed -i 's/\r$//' install-x-ui-auto.sh
+
+# Запуск модифицированного скрипта
 chmod +x install-x-ui-auto.sh
 echo -e "${green}Запускаю установку x-ui...${plain}"
 bash install-x-ui-auto.sh
